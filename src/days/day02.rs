1478d847ddedbc5c045de87c42e7c5a4d963f214
Sam Brougham <sam.brougham@gmail.com>
2025-03-11 18:42:57 -0400

    02 solution

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -1,10 +1,35 @@
 use std::{
+    collections::HashMap,
     fs::File,
-    io::{BufRead, BufReader, Result},
+    io::{BufRead, BufReader, Error, ErrorKind, Result},
 };
 
-pub fn solve() -> Result<String> {
-    let _lines = BufReader::new(File::open("./input/02.txt")?).lines();
+use chrono::DateTime;
 
-    Ok(format!("Not implemented!"))
+pub fn solve() -> Result<String> {
+    let lines = BufReader::new(File::open("./input/02.txt")?).lines();
+
+    let times = lines
+        .filter_map(|line| match line {
+            Ok(line) => Some(DateTime::parse_from_str(&line, "%+")),
+            Err(_) => None,
+        })
+        .filter_map(|dt| dt.ok())
+        .fold(HashMap::new(), |mut map, datetime| {
+            *map.entry(datetime).or_insert(0) += 1;
+            map
+        });
+
+    let wave_time = times
+        .iter()
+        .find_map(|(k, v)| if *v == 4 { Some(k) } else { None });
+
+    if let Some(time) = wave_time {
+        return Ok(format!(
+            "The gravitational wave was recorded at {}",
+            time.to_utc().format("%+")
+        ));
+    }
+
+    Err(Error::new(ErrorKind::NotFound, "No matching times found"))
 }

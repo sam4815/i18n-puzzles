036a8659ac0829c4350281909345ee01b4039ac7
sam <sam.brougham@gmail.com>
2025-03-18 07:54:44 -0400

    08 solution

--- src/days/day08.rs
+++ src/days/day08.rs
@@ -1,10 +1,37 @@
 use std::{
+    collections::HashMap,
     fs::File,
     io::{BufRead, BufReader, Result},
 };
 
-pub fn solve() -> Result<String> {
-    let _lines = BufReader::new(File::open("./input/08.txt")?).lines();
+use unidecode::unidecode;
 
-    Ok(format!("Not implemented!"))
+pub fn solve() -> Result<String> {
+    let lines = BufReader::new(File::open("./input/08.txt")?).lines();
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+
+    let valid_passwords = lines.map_while(Result::ok).filter(|line| {
+        let len = line.chars().count();
+        let normalized = unidecode(&line.to_lowercase());
+
+        (4..=12).contains(&len)
+            && line.chars().any(|c| c.is_ascii_digit())
+            && normalized.chars().any(|c| vowels.contains(&c))
+            && normalized
+                .chars()
+                .any(|c| c.is_alphabetic() && !vowels.contains(&c))
+            && normalized
+                .chars()
+                .fold(HashMap::new(), |mut map, c| {
+                    *map.entry(c).or_insert(0) += 1;
+                    map
+                })
+                .iter()
+                .all(|(_, v)| *v == 1)
+    });
+
+    Ok(format!(
+        "There are {} valid passwords",
+        valid_passwords.count()
+    ))
 }

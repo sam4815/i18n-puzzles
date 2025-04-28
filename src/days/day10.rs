0dda06ef2823915c489922c0d6df2eea3c70d3cb
sam <sam.brougham@gmail.com>
2025-04-28 08:46:04 -0400

    Clean up with itertools

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -2,27 +2,29 @@ extern crate bcrypt;
 extern crate unicode_normalization;
 
 use bcrypt::verify;
+use itertools::Itertools;
 use unicode_normalization::UnicodeNormalization;
 
 use std::{
-    collections::HashMap, fs::File, io::{BufReader, Read, Result}
+    collections::HashMap,
+    fs::File,
+    io::{BufReader, Read, Result},
 };
 
-pub fn find_permutations(str: String) -> Vec<String> {
-    let mut perms = vec!["".to_string()];
+pub fn generate_permutations(str: &str) -> impl Iterator<Item = String> {
+    str.nfc()
+        .map(|ch| {
+            let composed = ch.to_string();
+            let decomposed = ch.nfd().collect::<String>();
 
-    for char in str.chars().rev() {
-        let decomposition = char.nfd().collect::<String>();
-        let mut guaranteed = perms.iter().map(|p| decomposition.clone() + p).collect::<Vec<String>>();
-
-        if decomposition.len() > 1 {
-            guaranteed.extend(perms.iter().map(|p| char.to_string() + p).collect::<Vec<String>>());
-        }
-
-        perms = guaranteed
-    }
-
-    perms
+            if decomposed == composed {
+                vec![composed]
+            } else {
+                vec![composed, decomposed]
+            }
+        })
+        .multi_cartesian_product()
+        .map(|parts| parts.concat())
 }
 
 pub fn solve() -> Result<String> {
@@ -30,25 +32,33 @@ pub fn solve() -> Result<String> {
     BufReader::new(File::open("./input/10.txt")?).read_to_string(&mut input)?;
 
     let (hashed, attempts) = input.trim().split_once("\n\n").unwrap();
-    let hash_map: HashMap<String, String> = hashed.split("\n").fold(
-        HashMap::<String, String>::new(),
-        |mut map, line| {
-            let (username, password) = line.split_once(" ").unwrap();
-            map.insert(username.to_string(), password.to_string());
-            map
-        },
-    );
 
-    let valid_attempts = attempts.split("\n").filter(|line| {
-            println!("Starting");
-            let (username, attempt) = line.split_once(" ").unwrap();
+    let hash_map: HashMap<_, _> = hashed
+        .lines()
+        .filter_map(|line| line.split_once(' '))
+        .map(|(username, password)| (username.to_string(), password.to_string()))
+        .collect();
 
-            let permutations = find_permutations(attempt.to_string());
-            permutations.iter().any(|p| verify(p, hash_map.get(username).unwrap()).unwrap())
+    let mut matches: HashMap<String, String> = HashMap::new();
+
+    let valid_attempts = attempts.lines().filter(|line| {
+        let (username, attempt) = line.split_once(" ").unwrap();
+
+        if let Some(password) = matches.get(username) {
+            return password.nfc().eq(attempt.nfc());
         }
-    );
 
-    println!("{:?}", "È".nfd().collect::<String>());
+        for variant in generate_permutations(attempt) {
+            if let Some(hash) = hash_map.get(username) {
+                if verify(&variant, hash).unwrap_or(false) {
+                    matches.insert(username.to_string(), attempt.to_string());
+                    return true;
+                }
+            }
+        }
+
+        false
+    });
 
     Ok(format!(
         "There are {} valid login attempts.",
